34bb69673cf055bb0370637fa2bf3b426eb92aa0
natalia-yep_msi <epif97@mail.ru>
2025-02-01 19:48:31 +0300

    add vacancy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Создание экземпляра класса для работы с API сайтов с вакансиями
+from src.head_hunter_api import HeadHunterAPI
+from src.vacancy import Vacancy
+
 hh_api = HeadHunterAPI()
 
 # Получение вакансий с hh.ru в формате JSON
@@ -7,9 +10,11 @@ hh_vacancies = hh_api.get_vacancies("Python")
 # Преобразование набора данных из JSON в список объектов
 vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
 
-# Пример работы контструктора класса с одной вакансией
+# Пример работы конструктора класса с одной вакансией
 vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100 000-150 000 руб.", "Требования: опыт работы от 3 лет...")
 
+
+print(vacancy)
 # Сохранение информации о вакансиях в файл
 json_saver = JSONSaver()
 json_saver.add_vacancy(vacancy)
---
+++ src/vacancy.py
@@ -0,0 +1,39 @@
+class Vacancy:
+    vacancy_name: str
+    vacancy_url: str
+    salary: str
+    description: str
+
+    __slots__ = ('vacancy_name', 'vacancy_url', 'salary', 'description')
+
+    def __init__(self, vacancy_name, vacancy_url, salary, description):
+        self.vacancy_name = vacancy_name
+        self.vacancy_url = vacancy_url
+        self.salary = salary
+        self.description = description
+        self.__validate()
+
+    def __validate(self):
+        if not isinstance(self.vacancy_name, str) or not self.vacancy_name:
+            raise ValueError("Название вакансии должно быть указано")
+        if not isinstance(self.vacancy_url, str) or 'http' not in self.vacancy_url:
+            raise ValueError("Ссылка на вакансию должна быть корректной URL.")
+        if not isinstance(self.salary, str):
+            raise ValueError("Зарплата должна быть указана строкой")
+        if not isinstance(self.description, str):
+            raise ValueError("Описание должно быть строкой.")
+        if not self.salary:
+            self.salary = 'Зарплата не указана'
+
+    def __lt__(self, other):
+        if not isinstance(other, Vacancy):
+            return NotImplemented
+        return self.salary < other.salary
+
+    def __gt__(self, other):
+        if not isinstance(other, Vacancy):
+            return NotImplemented
+        return self.salary > other.salary
+
+
+
