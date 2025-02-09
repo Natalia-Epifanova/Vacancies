33e1234792ef9af8cbbcd1b6a1a7ad976bb19703
natalia-yep_msi <epif97@mail.ru>
2025-02-09 18:05:21 +0300

    add test_utils

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,76 @@ from src.vacancy import Vacancy
 
 @pytest.fixture()
 def first_vacancy():
-    return Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100000 - 150000 руб.", "Требования: опыт работы от 3 лет...")
+    return Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100000 - 150000 руб.",
+                   "Требования: опыт работы от 3 лет...")
 
 
 @pytest.fixture
 def second_vacancy():
-    return Vacancy("Java Developer", "https://hh.ru/vacancy/123457", "120000 - 150000 руб.", "Требования: опыт работы от 3 лет...")
+    return Vacancy("Java Developer", "https://hh.ru/vacancy/123457", "120000 - 150000 руб.",
+                   "Требования: опыт работы от 3 лет...")
+
+
+@pytest.fixture()
+def vacancy_dict():
+    return {"name": "Python Developer",
+            "alternate_url": "<https://hh.ru/vacancy/123456>",
+            "salary": "100000 - 150000 руб.",
+            "snippet": "Требования: опыт работы от 3 лет..."}
+
+
+@pytest.fixture()
+def salary_test():
+    return "100000 - 150000"
+
+
+
+@pytest.fixture()
+def vacancy_from_hh():
+    return {"id": "93353083",
+            "name": "Тестировщик комфорта квартир",
+            "salary": {
+                "from": 350000,
+                "to": 450000,
+                "currency": "RUR",
+                "gross": False
+            },
+            "snippet": {
+                "requirement": "Занимать активную жизненную позицию...",
+                "responsibility": "Оценивать вид из окна: встречать рассветы на кухне..."
+            },
+            "alternate_url": "https://hh.ru/vacancy/93353083"
+            }
+@pytest.fixture()
+def vacancy_half_of_params():
+    return {"id": "93353083",
+            "name": "Тестировщик комфорта квартир",
+            "salary": {
+                "from": 350000,
+                "to": None,
+                "currency": "RUR",
+                "gross": False
+            },
+            "snippet": {
+                "requirement": "Занимать активную жизненную позицию...",
+                "responsibility": None
+            },
+            "alternate_url": "https://hh.ru/vacancy/93353083"
+            }
+
+@pytest.fixture()
+def vacancy_half_of_params_2():
+    return {"id": "93353083",
+            "name": "Тестировщик комфорта квартир",
+            "salary": {
+                "from": None,
+                "to": 450000,
+                "currency": "RUR",
+                "gross": False
+            },
+            "snippet": {
+                "requirement": None,
+                "responsibility": "Оценивать вид из окна: встречать рассветы на кухне..."
+            },
+            "alternate_url": "https://hh.ru/vacancy/93353083"
+            }
---
+++ tests/test_utils.py
@@ -0,0 +1,59 @@
+from src.utils import vac_to_dict, dict_to_vac, type_of_salary, type_of_description, get_min_salary
+from src.vacancy import Vacancy
+
+
+
+
+def test_vac_to_dict(first_vacancy):
+    result = vac_to_dict(first_vacancy)
+    assert result == {"name": "Python Developer",
+        "alternate_url": "<https://hh.ru/vacancy/123456>",
+        "salary": "100000 - 150000 руб.",
+        "snippet": "Требования: опыт работы от 3 лет..."}
+
+def test_dict_to_vac(vacancy_dict):
+    result = dict_to_vac(vacancy_dict)
+    assert result.vacancy_name == "Python Developer"
+    assert result.vacancy_url == "<https://hh.ru/vacancy/123456>"
+    assert result.salary == "100000 - 150000 руб."
+    assert result.description == "Требования: опыт работы от 3 лет..."
+
+def test_get_min_salary(salary_test):
+    assert get_min_salary(salary_test) == 100000.0
+
+def test_type_of_salary(vacancy_from_hh):
+    result = type_of_salary(vacancy_from_hh)
+    assert result == "350000 - 450000"
+
+
+def test_type_of_description(vacancy_from_hh):
+    result = type_of_description(vacancy_from_hh)
+    assert result == 'Требования: Занимать активную жизненную позицию.... \nОбязанности: Оценивать вид из окна: встречать рассветы на кухне...'
+
+def test_type_of_salary_from(vacancy_half_of_params):
+    result = type_of_salary(vacancy_half_of_params)
+    assert result == "350000"
+
+def test_type_of_description_requirement(vacancy_half_of_params):
+    result = type_of_description(vacancy_half_of_params)
+    assert result == 'Требования: Занимать активную жизненную позицию....'
+
+
+def test_type_of_salary_to(vacancy_half_of_params_2):
+    result = type_of_salary(vacancy_half_of_params_2)
+    assert result == "450000"
+
+def test_type_of_description_responsibility(vacancy_half_of_params_2):
+    result = type_of_description(vacancy_half_of_params_2)
+    assert result == 'Обязанности: Оценивать вид из окна: встречать рассветы на кухне...'
+
+
+def test_get_min_salary_no_digits():
+    result = get_min_salary("Зарплата не указана")
+    assert result == float("inf")
+
+
+def test_get_min_salary_empty_string():
+    # Тестируем пустую строку
+    result = get_min_salary("")
+    assert result == float("inf")
